1c6cb6f6816008f01897df0b7301a50d3c67b3c4
ashna575 <ashnaraikwar9@gmail.com>
2025-07-17 19:30:55 +0000

    Datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all JS code as newer version
+
+let name = "ashna"
+let age =21
+let isLoggedIn = false
+
+//number
+//Bigint  string  Boolean =>t/f
+//null => standalone value  type objec 
+//symbol => unique
+//undefined
+
+
+//object
+console.log(typeof  null);
---
+++ 01_Basics/var.js
@@ -0,0 +1,10 @@
+const accountId = 14443
+let accountEmail = "xyz@gmail.com"
+var accountPass = "asdfg"
+accountCity = "pune"
+let accountState;
+
+accountCity ="mumbai"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPass,accountCity,accountState])
