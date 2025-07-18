b3a1eebc2c2f89236a1a06b4562c91383c10c7b4
ashna575 <ashnaraikwar9@gmail.com>
2025-07-18 08:22:05 +0000

    conversion

---
+++ 01_Basics/datatypesConversion.js
@@ -0,0 +1,21 @@
+let score ="33"
+console.log(typeof score);
+
+
+let vakueNo = Number(score)
+console.log(typeof vakueNo);
+
+//"33"=> 33
+//"33abc"=> NaN
+// true =>1 ; false =.0
+
+let positive="67 "
+let testBOolean = Boolean(positive)
+console.log(testBOolean)
+
+//""=> false
+//"  "=>true
+
+let height=88
+let stringg= String(height)
+console.log(typeof stringg)
