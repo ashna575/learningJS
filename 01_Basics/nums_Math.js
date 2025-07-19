af79433e7a829fd4198c5b44af3f4871eb962b72
ashna575 <ashnaraikwar9@gmail.com>
2025-07-19 20:02:26 +0000

    math

--- 01_Basics/nums_Math.js
+++ 01_Basics/nums_Math.js
@@ -1,1 +1,28 @@
-const balance = 400
+// const balance = 400
+
+// console.log(balance);
+
+// const score = new Number(100)
+// console.log(score);
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));
+
+// const decimalNo = 937.443554
+
+// console.log(decimalNo.toPrecision(4));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString);
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.round(3.6));
+console.log(Math.ceil(3.6));
+console.log(Math.abs(-3));
+console.log(Math.floor(3.6));
+
+
+console.log(Math.random());
