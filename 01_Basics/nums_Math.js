f35507effd12c494526855f822bc6bb3f7b6f3df
ashna575 <ashnaraikwar9@gmail.com>
2025-07-19 18:23:27 +0000

    num_maths

---
+++ 01_Basics/nums_Math.js
@@ -0,0 +1,1 @@
+const balance = 400
--- 01_Basics/string.js
+++ 01_Basics/string.js
@@ -2,14 +2,14 @@ const name = "ashna"
 const repoCount = 30
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
-const gameName = new String('Dreamhouse-01')
+const gameName = new String('Dreamhouse-01-sdf-skd')
 
-// console.log(gameName[0]);
-// console.log(gameName.length);
-// console.log(gameName.__proto__);
-// console.log(gameName.toUpperCase());
-// console.log(gameName.charAt(2));
-// console.log(gameName.indexOf('m'));
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('m'));
 
 const newgame = gameName.substring(0,3)
 console.log(newgame);
@@ -17,6 +17,7 @@ console.log(newgame);
 const anotherStr = gameName.slice(-2)
 console.log(anotherStr);
 
+console.log(gameName.split('-'));
 
 
 
