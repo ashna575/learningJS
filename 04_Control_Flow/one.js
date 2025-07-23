4e93893aefb4fea26f1986550edaa858ab2d5ff7
ashna575 <ashnaraikwar9@gmail.com>
2025-07-23 20:17:55 +0000

    controlFlow

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,29 @@
+//if
+// const isUserlogedin = true
+// if(isUserlogedin){
+// console.log("activated");
+
+// }
+
+const score =344
+if (score<50) {
+    const power = "FLy"
+    console.log(power);
+    
+}
+else {
+console.log("cant fly");
+
+}
+
+//shorthand notation
+const Balance= 1000
+//if(Balance>500)console.log();
+
+const isUserlogedin = true
+const debiyCard = true
+
+if(isUserlogedin && debiyCard){
+    console.log("allow to buy course");
+    
+}
