61dc4738dca46505d4c4c57763dfd3039d9db421
ashna575 <ashnaraikwar9@gmail.com>
2025-07-24 17:15:18 +0000

    controlFlow

--- 04_Control_Flow/one.js
+++ 04_Control_Flow/one.js
@@ -27,3 +27,25 @@ if(isUserlogedin && debiyCard){
     console.log("allow to buy course");
     
 }
+
+const month =3
+
+switch (month) {
+    case 1:
+        console.log("jan");
+         break;
+    case 2:
+        console.log("Feb");
+         break;
+    case 3:
+        console.log("march");
+         break;
+    case 4:
+        console.log("april");
+         break;""
+
+    default:
+        console.log("enter valid no");
+        
+        break;
+}
