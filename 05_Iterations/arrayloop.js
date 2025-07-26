e45a322d3b2188f608bce51d91fbbb099cb93bdf
ashna575 <ashnaraikwar9@gmail.com>
2025-07-26 18:13:51 +0000

    iterations

--- 05_Iterations/arrayloop.js
+++ 05_Iterations/arrayloop.js
@@ -15,7 +15,24 @@ map.set("fr","france")
 
 for (const [key, value] of map) {
     console.log(key ,':-', value  );
-    
-    
 }
 
+
+const programming = ["js","rb","py","java","cpp"]
+
+// coding.array.forEach(element => {
+    
+// });
+
+const values=coding.forEach(function(item) {
+    console.log(item);
+    return item
+}
+    
+);
+//console.log(alues)
+
+
+const myNums =[1,2,3,4,5,6,7,8,9,10]
+ const newnum = myNums.filter((num)=> num > 4)
+console.log(newnum);
---
+++ 05_Iterations/two.js
@@ -0,0 +1,10 @@
+//8
+const myNums=[1,2,3,4,5]
+
+
+//const newnum = myNums.map((i)=> i+10)
+const newNums = myNums.map((i)=> i *10).
+map((j)=> j+1)
+
+console.log(newNums);
+
